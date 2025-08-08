191acc060c39cddfb02e52fbcaa5b2b2cd7030d9
Derek Antrican <derekantrican@gmail.com>
2025-08-07 22:11:31 -0700

    Changes to fix centering for dialogs

--- client/src/components/DialogBase.js
+++ client/src/components/DialogBase.js
@@ -14,7 +14,7 @@ function DialogBase({isOpen, onClose, title, children, buttons, dialogStyle, chi
 
   return (
     <dialog ref={dialogRef} onClose={onClose}
-      style={{width: 'min(720px, 100vw - 20px)', maxHeight: '90vh', borderRadius: 6, padding: 0, borderWidth: 1, color: 'inherit', overflow: 'hidden',
+      style={{width: 'min(720px, 100vw - 20px)', maxHeight: '90vh', borderRadius: 6, padding: 0, margin: 0, borderWidth: 1, color: 'inherit', overflow: 'hidden',
               position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', ...dialogStyle}}>
       <div style={{display: 'flex', flexDirection: 'column', backgroundColor: '#2a2a2a', width: '100%', height: '100%', maxHeight: 'inherit'}}>
         <button style={{position: 'absolute', top: 0, right: 0, width: 60, height: 60}} onClick={onClose}>
