377e092ecde0c806cbd81b45086129ea4704971e
Dnl Rsr <rieser.daniel@gmail.com>
2025-09-05 22:04:30 +0200

    feat: replace anchor tags with Link components in Sidebar for better routing

--- client/src/components/layout/sidebar/Sidebar.tsx
+++ client/src/components/layout/sidebar/Sidebar.tsx
@@ -6,6 +6,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import styles from './Sidebar.module.scss';
 
@@ -22,36 +23,36 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen: _, onItemClick }) => {
       <nav className={styles.nav}>
         <ul className={styles.list}>
           <li className={styles.item}>
-            <a href="/" className={styles.link} onClick={onItemClick}>
+            <Link to="/" className={styles.link} onClick={onItemClick}>
               <span className={styles.icon}>
                 <FontAwesomeIcon icon={faPlay} />
               </span>
               <span>{t('sidebar.playlists')}</span>
-            </a>
+            </Link>
           </li>
           <li className={styles.item}>
-            <a href="/add" className={styles.link} onClick={onItemClick}>
+            <Link to="/add" className={styles.link} onClick={onItemClick}>
               <span className={styles.icon}>
                 <FontAwesomeIcon icon={faPlus} />
               </span>
               <span>{t('sidebar.addNew')}</span>
-            </a>
+            </Link>
           </li>
           <li className={styles.item}>
-            <a href="/activity" className={styles.link} onClick={onItemClick}>
+            <Link to="/activity" className={styles.link} onClick={onItemClick}>
               <span className={styles.icon}>
                 <FontAwesomeIcon icon={faChartPie} />
               </span>
               <span>{t('sidebar.activity')}</span>
-            </a>
+            </Link>
           </li>
           <li className={styles.item}>
-            <a href="/settings" className={styles.link} onClick={onItemClick}>
+            <Link to="/settings" className={styles.link} onClick={onItemClick}>
               <span className={styles.icon}>
                 <FontAwesomeIcon icon={faGear} />
               </span>
               <span>{t('sidebar.settings')}</span>
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
